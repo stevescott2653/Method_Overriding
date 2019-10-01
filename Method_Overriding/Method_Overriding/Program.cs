eb7754a0a37f619124526bd8de7b7495a02df88b
Steven Scott <stevescott2653@gmail.com>
2019-10-01 16:32:19 -0400

    Add files via upload

---
+++ Method_Overriding/Method_Overriding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class Animal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating dog food...");
+        }
+    }
+
+    public class TestOverriding
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+            d.Eat();
+        }
+    }
+}
